c53f3428159007165c54136465c1b21e73ff68ae
Muyiwa Oyinloye <maoja37@gmail.com>
2022-05-22 15:55:55 +0100

    created a layout builder to switch layouts for desktop screen or phone screen depending on width size

--- lib/screens/responsive_layou.dart
+++ lib/screens/responsive_layou.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'desktop_screen_layout.dart';
+import 'phone_screen_layout.dart';
+
 class ResponsiveLayout extends StatefulWidget {
   const ResponsiveLayout({ Key? key }) : super(key: key);
 
@@ -10,8 +13,13 @@ class ResponsiveLayout extends StatefulWidget {
 class _ResponsiveLayoutState extends State<ResponsiveLayout> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return LayoutBuilder(builder: (context, constraints){
+      if(constraints.maxWidth >= 1440){
+        return const DesktopScreenLayout();
+      }else{
+        return const PhoneScreenLayout();
+      }
+    
+    });
   }
 }
